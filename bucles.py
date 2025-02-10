1c777704eea2f6ca510c1170cddc31310e654a74
ionelectro <hugomgp2011@hotmail.com>
2025-02-10 20:50:41 +0100

    bucles.py

---
+++ bucles.py
@@ -0,0 +1,10 @@
+print ("Ejemplos de bucles")
+
+print("Bucle while")
+
+# contador con while
+contador = 0
+while contador <=5:
+    print(f"El contador es {contador}")
+    contador += 1
+print("Fin del bucle while")
